aed006b72fe57ae5a5256c48724c0946399c8a52
SirDarthBacon <33043494+SirDarthBacon@users.noreply.github.com>
2017-12-16 11:21:46 -0700

    initialcommit

---
+++ Robot_Control_Code/ftc_app-master_v3.5/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc11587/Autonomous_All_Blue.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name:"Auto_ColorBallOnly_Blue", group "Relic Recovery")
+@Disabled
+
+public class AutonomousDrive_Linear extends LinearOpMode {
+}
